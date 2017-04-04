fc333324c3a9486bce71f4512f2be2d0ae5f3d97
Sacha Schmid <schmid.sacha@gmail.com>
2017-04-04 17:23:15 +0200

    Add controllable player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public float maxSpeed = 10f;
+	bool facingRight = true;
+
+	private Rigidbody2D rb2D;
+
+	void Awake()
+	{
+		rb2D = GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate()
+	{
+		float move = Input.GetAxis ("Horizontal");
+		rb2D.velocity = new Vector2 (move * maxSpeed, rb2D.velocity.y);
+
+		if (move > 0 && !facingRight) {
+			Flip ();
+		} else if (move < 0 && facingRight) {
+			Flip ();
+		}
+	}
+
+	void Flip()
+	{
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}	
+}
